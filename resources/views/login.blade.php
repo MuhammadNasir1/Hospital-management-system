dd7353634af3fa906e665ef91979d9ee1029428f
M-ArhamWaheed <info.epage.org@gmail.com>
2024-07-19 13:52:21 +0500

    Add Button

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,6 +10,50 @@
     <link rel="shortcut icon" href="{{ asset('images/favicon(32X32).png') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <style>
+        .toggle-switch {
+            display: inline-block;
+            width: 40px;
+            height: 20px;
+            position: relative;
+        }
+
+        .toggle-switch input {
+            display: none;
+        }
+
+        .slider {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            transition: .4s;
+            border-radius: 20px;
+        }
+
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 14px;
+            width: 14px;
+            left: 3px;
+            bottom: 3px;
+            background-color: white;
+            transition: .4s;
+            border-radius: 50%;
+        }
+
+        input:checked+.slider {
+            background-color: #4f46e5;
+        }
+
+        input:checked+.slider:before {
+            transform: translateX(20px);
+        }
+    </style>
 </head>
 
 <body class="flex justify-center items-center min-h-[100vh]">
@@ -45,12 +89,22 @@
                                 placeholder="Password Here">
                         </div>
 
+                        <div class="mt-3 flex items-center">
+                            <label class="toggle-switch">
+                                <input type="checkbox" id="rememberMeCheckbox">
+                                <span class="slider"></span>
+                            </label>
+                            <label for="rememberMeCheckbox" class="ml-2 text-gray-700 font-medium">Remember me</label>
+                        </div>
                         <div class="flex items-center flex-col mt-3">
                             <button id="loginbutton"
                                 class="w-[55%] gradient-bakground active:text-black active:w-[60%] duration-150 mx-auto mt-6 py-3 rounded-full text-white uppercase">Login</button>
                             <p class="text-[#000000] mt-3 opacity-[50%]">Create an Account <a href="../register"
                                     class="text-blue-600">Register</a></p>
                         </div>
+
+
+
                     </form>
                 </div>
             </div>
@@ -65,6 +119,13 @@
 
 
     <script>
+        document.getElementById('rememberMeCheckbox').addEventListener('change', function() {
+            if (this.checked) {
+                console.log("Remember Me toggled on");
+            } else {
+                console.log("Remember Me toggled off");
+            }
+        });
         $(document).ready(function() {
             $("#login_data").submit(function(event) {
                 event.preventDefault();
