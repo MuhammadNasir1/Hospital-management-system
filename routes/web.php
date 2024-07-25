0fde3b1ff157c2b7f7e91e47dfa3733b56ccca41
MuhammadNasir1 <nmian7080@gmail.com>
2024-07-25 11:57:23 +0500

    add route

--- routes/web.php
+++ routes/web.php
@@ -58,3 +58,11 @@ Route::get('/pharmacy/inventory', [InventoryController::class, 'index']);
 
 
 Route::post('/addInventroy', [InventoryController::class, 'insert']);
+
+
+Route::get('/pharmacy/inventory', function () {
+    return view('pharmacy.inventory');
+});
+Route::get('/pharmacy/create-invoice', function () {
+    return view('pharmacy.invoice');
+});
