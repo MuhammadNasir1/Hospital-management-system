62994c3b7f001ad5ffb97eb08691b155125bc4f0
M-ArhamWaheed <info.epage.org@gmail.com>
2024-07-27 17:10:54 +0500

    V1

--- resources/views/pharmacy/invoice.blade.php
+++ resources/views/pharmacy/invoice.blade.php
@@ -83,7 +83,6 @@
                                                 <option value="{{ $data->id }}">
                                             @endforeach
                                         </datalist>
-                                        {{--  --}}
 
                                     </td>
                                     <td class="border border-[#DEE2E6] whitespace-nowrap px-3">
